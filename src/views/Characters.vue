dcc033eea30b8686c21280ec8600ee14a6f591a8
xetra11 <xetra11@outlook.com>
2019-04-14 22:03:12 +0200

    feat(character): add create new character button

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -8,31 +8,51 @@
     </template>
 
     <v-tab-item v-for="(character, index) in characters" :key="index" :value="'tab-' + index">
-      <v-container>
-        <v-layout column align-center>
-          <v-flex xs12>
-            <v-avatar class="align-end" size="150" color="rgba(85,85,85,1)">
-              <v-icon size="50">perm_identity</v-icon>
-            </v-avatar>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-container>
-        <v-layout>
-          <v-flex xs12>
-            <v-card dark color="rgba(75,75,75,1)">
-              <v-card-title><h4>Details</h4></v-card-title>
-              <v-divider></v-divider>
-              <v-list dense>
-                <v-list-tile v-for="(value,key) in character">
-                  <v-list-tile-content>{{key}}</v-list-tile-content>
-                  <v-list-tile-content class="align-end">{{value}}</v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <template v-if="!emptyObject(character)">
+        <v-container>
+          <v-layout column align-center>
+            <v-flex xs12>
+              <v-avatar class="align-end" size="150" color="rgba(85,85,85,1)">
+                <v-icon size="50">perm_identity</v-icon>
+              </v-avatar>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-container>
+          <v-layout>
+            <v-flex xs12>
+              <v-card dark color="rgba(75,75,75,1)">
+                <v-card-title><h4>Details</h4></v-card-title>
+                <v-divider></v-divider>
+                <v-list dense>
+                  <v-list-tile v-for="(value,key) in character">
+                    <v-list-tile-content>{{key}}</v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{value}}</v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </template>
+      <template v-else>
+        <v-container>
+          <v-layout dark column align-center>
+            <v-flex xs12>
+              <v-avatar class="align-end" size="150" color="rgba(85,85,85,1)">
+                <v-icon size="50">far fa-question-circle</v-icon>
+              </v-avatar>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-container>
+          <v-layout dark column align-center>
+            <v-flex xs12>
+              <v-btn dark color="rgba(75,75,75,1)">Create New Character</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </template>
     </v-tab-item>
   </v-tabs>
 </template>
