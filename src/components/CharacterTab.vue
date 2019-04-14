22ca0aefb6e5f222f32fe08c1f938e25c964c3f9
xetra11 <xetra11@outlook.com>
2019-04-14 21:46:55 +0200

    refactor(character): extract tabs as component

---
+++ src/components/CharacterTab.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-tab :href="'#tab-' + index">
+      {{name}}
+      <v-icon>{{icon}}</v-icon>
+    </v-tab>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator";
+
+  @Component({})
+  export default class CharacterTab extends Vue {
+    @Prop() private index!: number;
+    @Prop() private name!: object;
+    @Prop() private icon!: string;
+  }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,21 +1,15 @@
 <template>
   <v-tabs centered color="rgba(75,75,75,1)" dark icons-and-text>
-    <v-tabs-slider color="yellow"></v-tabs-slider>
+    <v-tabs-slider color="white"></v-tabs-slider>
 
-    <v-tab v-for="(character, index) in characters" :href="'#tab-' + index">
-      <template v-if="!emptyObject(character)">
-        {{character.name}}
-        <v-icon>account_box</v-icon>
-      </template>
-      <template v-else>
-        {{empty.name}}
-        <v-icon>far fa-question-circle</v-icon>
-      </template>
-    </v-tab>
+    <template v-for="(character, index) in characters">
+      <character-tab v-if="!emptyObject(character)" :index="index" :name="character.name" icon="account_box"/>
+      <character-tab v-else :index="index" :name="empty.name" icon="far fa-question-circle"/>
+    </template>
 
     <v-tab-item v-for="(character, index) in characters" :key="index" :value="'tab-' + index">
       <v-container>
-        <v-layout column align-center >
+        <v-layout column align-center>
           <v-flex xs12>
             <v-avatar class="align-end" size="150" color="rgba(85,85,85,1)">
               <v-icon size="50">perm_identity</v-icon>
@@ -30,7 +24,7 @@
               <v-card-title><h4>Details</h4></v-card-title>
               <v-divider></v-divider>
               <v-list dense>
-                <v-list-tile v-for="(value,key) in character" >
+                <v-list-tile v-for="(value,key) in character">
                   <v-list-tile-content>{{key}}</v-list-tile-content>
                   <v-list-tile-content class="align-end">{{value}}</v-list-tile-content>
                 </v-list-tile>
@@ -43,33 +37,33 @@
   </v-tabs>
 </template>
 <script lang="ts">
-import {Component, Prop, Vue} from "vue-property-decorator";
-import Character from "@/components/Character.vue";
+    import {Component, Prop, Vue} from "vue-property-decorator";
+    import CharacterTab from "@/components/CharacterTab.vue";
 
-@Component({Character})
-export default class Characters extends Vue {
-    
-    private empty: object = {
-        name: "No Character"
-    };
-    
-    private characters: array = [
-        {
-            name: "Sgt.Spinelli",
-            class: "Group Leader",
-            reputation: 100
-        },
-        {
-            name: "Mr.Nice",
-            class: "Medic",
-            reputation: 50
-        },
-        {
+    @Component({components: {CharacterTab}})
+    export default class Characters extends Vue {
+
+        private empty: object = {
+            name: "empty"
+        };
+
+        private characters: object[] = [
+            {
+                name: "Sgt.Spinelli",
+                class: "Group Leader",
+                reputation: 100
+            },
+            {
+                name: "Mr.Nice",
+                class: "Medic",
+                reputation: 50
+            },
+            {}
+        ];
+
+        private emptyObject(obj: object): boolean {
+            return Object.entries(obj).length === 0 && obj.constructor === Object;
         }
-    ];
-    private emptyObject(obj): boolean {
-        return Object.entries(obj).length === 0 && obj.constructor === Object
-    }
 
-}
+    }
 </script>
