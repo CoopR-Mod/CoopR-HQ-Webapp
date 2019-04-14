e59016b2f02b182ae07c73db00bca69b055e7cb9
xetra11 <xetra11@outlook.com>
2019-04-14 16:30:01 +0000

    refactor(news): move the file

---
+++ src/views/News.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container grid-list-xs text-xs-left>
+    <v-layout row wrap>
+      <v-flex lg3 xs12>
+        <v-card dark color="rgba(60,60,60,1)">
+          <v-img :src="require('../assets/coopr_logo_1.png')" class="my-3" contain ></v-img>
+          <v-card-title primary-title>
+            <h3 class="headline">News</h3>
+          </v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</v-card-text>
+        </v-card>
+        <v-card dark color="rgba(60,60,60,1)">
+          <v-img :src="require('../assets/coopr_logo_1.png')" class="my-3" contain ></v-img>
+          <v-card-title primary-title>
+            <h3 class="headline text-md-center">News</h3>
+          </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "News"
+  }
+</script>
+
+<style scoped>
+
+</style>
