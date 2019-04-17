5d30da5706d5f6a5bd1a7f2243f0a2b79ae1f78e
xetra11 <xetra11@outlook.com>
2019-04-17 15:36:01 +0200

    refactor(character): revamp charater menu

--- src/components/CharacterMenu.vue
+++ src/components/CharacterMenu.vue
@@ -1,26 +1,15 @@
 <template>
   <v-container grid-list-md>
-    <v-layout row >
-      <v-flex xs4>
-          <v-card v-ripple dark> <v-card-title><v-icon class="pl-3"large>edit</v-icon></v-card-title> </v-card>
-      </v-flex>
-      <v-flex xs4>
-          <v-card v-ripple dark> <v-card-title ><v-icon class="pl-3"large>shield</v-icon></v-card-title> </v-card>
-      </v-flex>
-      <v-flex xs4>
-          <v-card v-ripple dark> <v-card-title><v-icon class="pl-3"large>phone</v-icon></v-card-title> </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row >
-      <v-flex xs4>
-        <v-card v-ripple dark> <v-card-title><v-icon class="pl-3"large>person</v-icon></v-card-title> </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card v-ripple dark> <v-card-title ><v-icon class="pl-3"large>group</v-icon></v-card-title> </v-card>
-      </v-flex>
-      <v-flex xs4>
-        <v-card v-ripple dark> <v-card-title><v-icon class="pl-3"large>star</v-icon></v-card-title> </v-card>
-      </v-flex>
+    <v-layout row wrap>
+      <template v-for="menu in menuIcons">
+        <v-flex xs4 >
+          <v-card v-ripple dark>
+            <v-card-title class="justify-center">
+              <v-icon large>{{menu.icon}}</v-icon>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </template>
     </v-layout>
   </v-container>
 </template>
@@ -30,6 +19,35 @@
 
     @Component({})
     export default class CharacterMenu extends Vue {
+
+
+        private menuIcons: object[] = [
+            {
+                icon: "phone",
+                url: ""
+            },
+            {
+                icon: "assignment_turned_in",
+                url: ""
+            },
+            {
+                icon: "alarm_on",
+                url: ""
+            },
+            {
+                icon: "autorenew",
+                url: ""
+            },
+            {
+                icon: "person",
+                url: ""
+            },
+            {
+                icon: "shield",
+                url: ""
+            },
+
+        ]
     }
 </script>
 
