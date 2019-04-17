c00e3caf555d6cf2c6eeb4a1b071f958507dc5e2
xetra11 <xetra11@outlook.com>
2019-04-17 13:43:12 +0200

    refactor(footer): resize social icons and add version tag

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,41 @@
 <template>
-  <v-footer app dark color="rgba(75,75,75,1)" class="pa-3">
-    <v-layout text-xs-center>
-      <v-flex row xs12>
-        <a class="no-underline" target="_blank"
-           href="https://www.youtube.com/watch?v=rsItYfsK0qo&list=PLk6AE-7-HXP5CUk7Wtg8bqSjD0D2DVAa8">
-          <v-icon>fab fa-youtube</v-icon>
-        </a>
-      </v-flex>
-      <v-flex row xs12>
-        <a class="no-underline" target="_blank" href="https://www.instagram.com/cooprmod/">
-          <v-icon>fab fa-instagram</v-icon>
-        </a>
-      </v-flex>
-      <v-flex row xs12>
-        <a class="no-underline" target="_blank" href="https://github.com/CoopR-Mod/CoopR-Mod">
-          <v-icon>fab fa-github</v-icon>
-        </a>
-      </v-flex>
+  <v-footer app dark color="rgba(75,75,75,1)" height="auto">
+    <v-layout row justify-space-around wrap>
+      <template v-for="social in socialLinks">
+        <v-flex xs2 text-xs-center>
+          <a class="no-underline" target="_blank" :href="social.url">
+            <v-icon class="pt-1" size="35px">{{ social.icon }}</v-icon>
+          </a>
+        </v-flex>
+      </template>
+      <v-flex xs12 class="pl-2"> alpha-v{{version}}-{{build}} </v-flex>
     </v-layout>
   </v-footer>
+
 </template>
 
-<script>
-  import {Component, Prop, Vue} from "vue-property-decorator";
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
 
-  @Component({})
-  export default class Footer extends Vue {
-  }
+    @Component({})
+    export default class Footer extends Vue {
+        private version: string = "0.0.1";
+        private build: number = process.env.VUE_APP_BUILD;
+        private socialLinks: object[] = [
+            {
+                url: "https://www.youtube.com/watch?v=rsItYfsK0qo&list=PLk6AE-7-HXP5CUk7Wtg8bqSjD0D2DVAa8",
+                icon: "fab fa-youtube",
+            },
+            {
+                url: "https://www.instagram.com/cooprmod/",
+                icon: "fab fa-instagram"
+            },
+            {
+                url: "https://github.com/CoopR-Mod/CoopR-Mod",
+                icon: "fab fa-github"
+            }
+        ]
+    }
 </script>
 
 <style scoped>
