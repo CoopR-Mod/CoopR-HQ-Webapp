4cd6f1debe21dcc252bf753da1311827af715aab
xetra11 <xetra11@outlook.com>
2019-04-17 11:14:49 +0200

    chore(app): add env configs

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -5,6 +5,7 @@
         <v-icon>perm_identity</v-icon>
       </v-avatar>
     </router-link>
+    <v-card-title>alpha-v{{version}}-{{build}}</v-card-title>
     <v-spacer></v-spacer>
     <v-spacer></v-spacer>
     <v-layout justify-space-between>
@@ -25,6 +26,8 @@
 
     @Component({})
     export default class ToolBar extends Vue {
+        private version: string = "0.0.1";
+        private build: number = process.env.VUE_APP_BUILD;
     }
 </script>
 
